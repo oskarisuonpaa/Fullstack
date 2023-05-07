398060896f90deeb91c07d6c31f2ea79658b7ba6
oskarisuonpaa <oskari.suonpaa@outlook.com>
2023-05-07 22:47:25 +0300

    added eslint

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -43,7 +43,7 @@ if (process.argv.length === 3) {
     number,
   });
 
-  person.save().then((result) => {
+  person.save().then(() => {
     console.log(`added ${name} number ${number} to phonebook`);
     mongoose.connection.close();
   });
